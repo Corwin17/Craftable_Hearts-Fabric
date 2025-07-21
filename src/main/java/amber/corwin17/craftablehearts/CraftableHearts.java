0800ea7777e2d07219407ccabbeedad34dbb840f
Corwin17 <tyler@corso.dog>
2025-07-21 15:48:08 -0700

    Initial commit

---
+++ src/main/java/amber/corwin17/craftablehearts/CraftableHearts.java
@@ -0,0 +1,16 @@
+package amber.corwin17.craftablehearts;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CraftableHearts implements ModInitializer {
+	public static final String MOD_ID = "craftablehearts";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		
+	}
+}
---
+++ src/main/java/amber/corwin17/craftablehearts/CraftableHeartsClient.java
@@ -0,0 +1,10 @@
+package amber.corwin17.craftablehearts;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CraftableHeartsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        
+    }
+}
